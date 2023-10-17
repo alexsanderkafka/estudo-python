62b4723a58a48c89d78dae4c62755ae543d4c5f8
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-10-17 16:31:59 -0300

    Continuando o jogo da forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,6 +3,26 @@ def jogar():
     print("Bem vindo ao jogo de Forca")
     print("********************************")
 
+    palavra_secreta = "banhia".upper()
+
+    enforcou = False
+    acertou = False
+
+
+    while (not enforcou and not acertou):
+
+        chute_user = input("Digite uma letra: ").upper().strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if (chute_user == letra):
+                print("Encontrei a letra {} na posição {}".format(
+                    chute_user, index))
+            index += 1
+
+        print("JOGANDO...")
+
     print("Fim do jogo")
 
 
--- jogos/tempCodeRunnerFile.py
+++ jogos/tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-
-    forca.jogar()
+banhia
